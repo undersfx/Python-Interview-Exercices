3e0f9eb78d4299ee6ed817e5cd41ce0e104536db
undersfx <atchin_@hotmail.com>
2020-02-28 19:39:21 -0300

    add: find duplicates problem

---
+++ find_dups.py
@@ -0,0 +1,34 @@
+'''
+Given a sorted array of integers "a". Find the first duplicated value.
+
+Tests:
+>>> a = [1,1,2,3,4]
+>>> find_duplicates(a)
+1
+
+>>> a = [1,2,3,4,4,5,6,7]
+>>> find_duplicates(a)
+4
+
+>>> a = [1,2,3,4,5,6,7,7]
+>>> find_duplicates(a)
+7
+
+Given a array of integers "a". Find the position of the first duplicated value.
+
+>>> a = [4,3,2,1,1]
+>>> find_duplicates_not_sorted(a)
+1
+
+>>> a = [1,2,3,4,4,5,6]
+>>> find_duplicates_not_sorted(a)
+4
+
+>>> a = [7,2,3,1,5,4,7]
+>>> find_duplicates_not_sorted(a)
+7
+
+'''
+
+def find_duplicates(arr):
+    pass
