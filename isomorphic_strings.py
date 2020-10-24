3ab5fa51bd97bdb5bef1f01556eebe6c86e0fc8f
undersfx <atchin_@hotmail.com>
2020-10-23 23:46:41 -0300

    add: isomorphic strings exercice

---
+++ isomorphic_strings.py
@@ -0,0 +1,46 @@
+"""
+Given two strings s and t, determine if they are isomorphic. Two strings are isomorphic if the characters in s can be replaced to get t.
+
+For example,"egg" and "add" are isomorphic, "foo" and "bar" are not.
+
+Docstrings:
+>>> is_isomorphic('egg', 'add')
+True
+
+>>> is_isomorphic('foo', 'bar')
+False
+
+>>> is_isomorphic('baba', 'bbbb')
+True
+
+>>> is_isomorphic('baba', 'abab')
+True
+
+>>> is_isomorphic('abacate', 'bbbcbte')
+True
+
+>>> is_isomorphic('abacate', 'babcbte')
+True
+
+>>> is_isomorphic('abc', 'ooo')
+True
+
+>>> is_isomorphic('a', 'ba')
+False
+"""
+
+def is_isomorphic(s, t):
+    if len(s) != len(t):
+        return False
+
+    correspondencia = {}
+    for key, char in enumerate(t):
+        if char != s[key]:
+            if not char in correspondencia.keys():
+                correspondencia[char] = s[key]
+
+    if len(correspondencia.keys()) != len(set(correspondencia.values())):
+        return False
+
+    return True
+
