b83816b8cc76b46fa49a1c8b552920315c5edd96
undersfx <atchin_@hotmail.com>
2021-02-07 00:27:36 -0300

    add: progress bar example

---
+++ playground/progress_bar.py
@@ -0,0 +1,47 @@
+"""
+Snippet adapted from this StackOverflow response: https://stackoverflow.com/a/34325723
+"""
+
+import time
+
+# Print iterations progress
+def printProgressBar(iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar.
+
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+
+    Example:
+    >>> items = list(range(0, 101))
+    >>> l = len(items)
+    >>> for i, item in enumerate(items):
+        ... # Do stuff...
+        ... printProgressBar(i + 1, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total:
+        print()
+
+
+if __name__ == '__main__':
+    # Example Usade
+    items = list(range(0, 57))
+    l = len(items)
+
+    # Initial call to print 0% progress
+    printProgressBar(0, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+    for i, item in enumerate(items):
+        time.sleep(0.1)
+        printProgressBar(i + 1, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
