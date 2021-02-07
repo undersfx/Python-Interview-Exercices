73ba017236a1e34cc8fb74e04f5cf15d6f22a009
undersfx <atchin_@hotmail.com>
2021-02-06 23:42:46 -0300

    add: tests to square and alice

--- square_root.py
+++ square_root.py
@@ -1,20 +1,44 @@
 """
 Square Root the hard way
+
+Tests:
+
+>>> find_square_root(0)
+0
+>>> find_square_root(1)
+0.9999923706054688
+>>> find_square_root(2)
+1.4142074584960938
+>>> find_square_root(3)
+1.732046127319336
+>>> find_square_root(4)
+2.0000076293945312
+>>> find_square_root(5)
+2.2360706329345703
+>>> find_square_root(6)
+2.4494876861572266
+>>> find_square_root(7)
+2.645754814147949
+>>> find_square_root(8)
+2.8284225463867188
+>>> find_square_root(9)
+2.999997138977051
+>>> find_square_root(10)
+3.1622791290283203
 """
 
 
 def find_square_root(x):
-
     """Calculates the square root of a value x"""
-    
-    precision = 10 ** -5
+    PRECISION = 10 ** -5
 
     # Choose two numbers in a way that sqrt(x) belongs to the interval
     s = 0
     e = int(x)
-  
-    # Find the root
-    while (e - s) > precision:
+    m = 0
+
+    # Calculation to find the square root
+    while (e - s) > PRECISION:
         # Find the middle
         m = (e + s) / 2
 
@@ -24,14 +48,13 @@ def find_square_root(x):
             s = m
 
     return m
-    
 
-inputs = []
+if __name__ == "__main__":
+    inputs = []
 
-for i in range(0, 11):
-    value = input()
-    inputs.append(value)
+    for i in range(0, 65):
+        inputs.append(i)
 
-for value in inputs:
-    square_root = find_square_root(value)
-    print('%.3f' % square_root)
+    for value in inputs:
+        square_root = find_square_root(value)
+        print('Square Root for % is equal to %.3f' % (value, square_root))
--- word_count_alice.py
+++ word_count_alice.py
@@ -1,32 +1,62 @@
-#Importa lib de string para utilizar as pontuações
-import string
+"""
+Count the number of occurrences of the string 'alice' in the book "Alice in Wonderland" (alice.txt)
+
+Tests:
+>>> text = "banana banana banana"
+>>> word = 'banana'
+>>> count_words(text, word)
+3
+
+>>> text = "a aba baba abababa aba"
+>>> word = 'aba'
+>>> count_words(text, word)
+2
+
+>>> count_words("a", "")
+-1
+
+>>> count_words("", "a")
+-1
+
+>>> count_words("", "")
+-1
+"""
+
+
+from string import punctuation
+
 
 WORD = 'alice'
 
-#Abre arquivo alice.txt em alice
-with open('alice.txt', encoding="utf8") as alice:
-
-        # Carrega o conteúdo do arquivo
-        texto = alice.read()
-
+def count_words(text, word):
         # Normaliza o texto o convertendo para minusculo
-        texto = texto.lower()
+        text = text.lower()
 
         # Normaliza a  pontuação substituindo por ' '
-        for x in string.punctuation:
-                texto = texto.replace(x, ' ')
+        for x in punctuation:
+                text = text.replace(x, ' ')
 
         # Quebra palavrras em uma list
-        texto = texto.split()
+        text = text.split()
+
+        if not text or not word: return -1
 
         # Dicionário de contagem
-        d = {}
+        result_dict = {}
 
-        for palavra in texto:
+        for s in text:
                 # Procura por contagem de palavras, caso não encontre retorna 0 + 1
-                d[palavra] = d.get(palavra, 0) + 1
+                result_dict[s] = result_dict.get(s, 0) + 1
 
-# Mostra o total somado correspondente ao indice esperado (WORD)
-print('A palavra "'+ WORD + '" aparece %s vezes.' %d[WORD])
+        return result_dict[word]
 
 
+if __name__ == "__main__":
+        #Abre arquivo alice.txt em alice
+        with open('alice.txt', encoding="utf8") as alice:
+                # Carrega o conteúdo do arquivo
+                texto = alice.read()
+                count = count_words(texto, WORD)
+
+                # Mostra o total somado correspondente ao indice esperado (WORD)
+                print('A palavra "'+ WORD + '" aparece %s vezes.' % count)
